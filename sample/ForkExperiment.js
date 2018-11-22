0f9dbebca6834bd1b78430933ced0ccbeb5b4cd0
vmigdal <volodymyr.migdal@gmail.com>
2018-11-22 16:55:33 +0200

    add fork sample

---
+++ sample/ForkExperiment.js
@@ -0,0 +1,24 @@
+
+if( typeof module !== 'undefined' )
+require( 'wexternalfundamentals' );
+require( 'wConsequence' );
+var _ = wTools;
+
+/**/
+
+// now working at all, because fork spawns node executable by default
+// _.shell({ path : 'node --max_old_space_size=1024  sample/Sample.js', mode : 'fork' })
+
+// invalid argument, because node expects only path to module
+// _.shell({ path : '--max_old_space_size=1024  sample/Sample.js', mode : 'fork' })
+
+// passing arguments for executable( node ) throught execArgv
+//https://nodejs.org/api/child_process.html#child_process_child_process_fork_modulepath_args_options
+
+var o = { path : 'sample/Sample.js', mode : 'fork', execArgv : [ '--max_old_space_size=1024'  ] }
+_.shell( o )
+.doThen( () =>
+{
+  console.log( o.process.spawnargs );
+  return o;
+})
