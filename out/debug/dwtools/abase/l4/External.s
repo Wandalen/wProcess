2d293e83b997d7b584abbe4d7c96adf26ecccf61
Wandalen <wandalen.me@gmail.com>
2019-08-01 09:50:26 +0300

    auto commit

--- out/debug/dwtools/abase/l4/External.s
+++ out/debug/dwtools/abase/l4/External.s
@@ -491,7 +491,7 @@ function shell_body( o )
       else
       {
         if( appPath.length )
-        _.assert( _.strSplitNonPreserving({ src : appPath }).length === 1, ' o.execPath must not contain arguments if those were provided through options' )
+        _.assert( _.strSplitNonPreserving({ src : appPath }).length === 1, ' o.execPath must not contain arguments if those were provided through options' );
       }
 
       let o2 = optionsForSpawn();
@@ -534,6 +534,20 @@ function shell_body( o )
     else _.assert( 0, 'Unknown mode', _.strQuote( o.mode ), 'to shell path', _.strQuote( o.paths ) );
 
   }
+
+/*
+qqq
+add coverage
+
+for combination:
+  path to exe file : [ with space, without space ]
+  execPath : [ has arguments, only path to exe file ]
+  args : [ has arguments, empty ]
+  mode : [ 'fork', 'exec', 'spawn', 'shell' ]
+
+example of execPath :
+  execPath : '"/dir with space/app.exe" firstArg secondArg:1 "third arg" \'fourth arg\'  `"fifth" arg`
+*/
 
   /* */
 
@@ -830,25 +844,17 @@ let shell = _.routineFromPreAndBody( shell_pre, shell_body );
 
 //
 
-// function shellPassingThrough( o )
-// {
-//   if( _.strIs( o ) )
-//   o = { execPath : o }
-//   _.routineOptions( shellPassingThrough, o );
-//   _.assert( arguments.length === 1, 'Expects single argument' );
-//   let result = _.shell( o );
-//   return result;
-// }
-
 let shellPassingThrough = _.routineFromPreAndBody( shell_pre, shell_body );
 
 var defaults = shellPassingThrough.defaults;
 
+defaults.verbosity = 0;
 defaults.passingThrough = 1;
 defaults.applyingExitCode = 1;
 defaults.throwingExitCode = 0;
 defaults.outputPiping = 1;
 defaults.stdio = 'inherit';
+// defaults.mode = 'spawn'; // xxx : uncomment after fix of the mode
 
 //
 
@@ -892,14 +898,9 @@ function shellNode_body( o )
   _.include( 'wPathFundamentals' );
   _.include( 'wFiles' );
 
-  // if( _.strIs( o ) )
-  // o = { execPath : o }
-
   _.assertRoutineOptions( shellNode, o );
   _.assert( _.strIs( o.execPath ) );
   _.assert( !o.code );
-  // _.accessor.forbid( o, 'child' );
-  // _.accessor.forbid( o, 'returnCode' );
   _.assert( arguments.length === 1, 'Expects single argument' );
 
   /*
@@ -950,6 +951,7 @@ defaults.passingThrough = 0;
 defaults.maximumMemory = 0;
 defaults.applyingExitCode = 1;
 defaults.stdio = 'inherit';
+defaults.mode = 'fork';
 
 let shellNode = _.routineFromPreAndBody( shell_pre, shellNode_body );
 
@@ -986,28 +988,17 @@ let shellNode = _.routineFromPreAndBody( shell_pre, shellNode_body );
  * @memberof module:Tools/base/ExternalFundamentals.Tools( module::ExternalFundamentals )
  */
 
-// function shellNodePassingThrough( o )
-// {
-//
-//   if( _.strIs( o ) )
-//   o = { execPath : o }
-//
-//   _.routineOptions( shellNodePassingThrough, o );
-//   _.assert( arguments.length === 1, 'Expects single argument' );
-//   let result = _.shellNode( o );
-//
-//   return result;
-// }
-
 let shellNodePassingThrough = _.routineFromPreAndBody( shell_pre, shellNode.body );
 
 var defaults = shellNodePassingThrough.defaults;
 
+defaults.verbosity = 0;
 defaults.passingThrough = 1;
 defaults.maximumMemory = 1;
 defaults.applyingExitCode = 1;
 defaults.throwingExitCode = 0;
 defaults.outputPiping = 1;
+defaults.mode = 'fork';
 
 //
 
@@ -1422,9 +1413,9 @@ function appExitCode( status )
 
   if( _global.process )
   {
+    result = process.exitCode || 0;
     if( status !== undefined )
     process.exitCode = status;
-    result = process.exitCode;
   }
 
   return result;
@@ -1472,12 +1463,12 @@ function appExitWithBeep( exitCode )
 //
 
 /*
-qqq : use maybe appRepairExitHandler instead of appRegisterExitHandler?
-qqq : investigate difference between appRepairExitHandler and appRegisterExitHandler
+qqq : use maybe appExitHandlerRepair instead of appExitHandlerOnce?
+qqq : investigate difference between appExitHandlerRepair and appExitHandlerOnce
 */
 
 let appRepairExitHandlerDone = 0;
-function appRepairExitHandler()
+function appExitHandlerRepair()
 {
 
   _.assert( arguments.length === 0 );
@@ -1506,6 +1497,23 @@ function appRepairExitHandler()
     }
   });
 
+  process.on( 'SIGTERM', function()
+  {
+    console.log( 'SIGTERM' );
+    try
+    {
+      process.exit();
+    }
+    catch( err )
+    {
+      console.log( 'Error!' );
+      console.log( err.toString() );
+      console.log( err.stack );
+      process.removeAllListeners( 'exit' );
+      process.exit();
+    }
+  });
+
   process.on( 'SIGUSR1', function()
   {
     console.log( 'SIGUSR1' );
@@ -1518,7 +1526,7 @@ function appRepairExitHandler()
       console.log( 'Error!' );
       console.log( err.toString() );
       console.log( err.stack );
-      process.removeListener( 'exit' );
+      process.removeAllListeners( 'exit' );
       process.exit();
     }
   });
@@ -1535,7 +1543,7 @@ function appRepairExitHandler()
       console.log( 'Error!' );
       console.log( err.toString() );
       console.log( err.stack );
-      process.removeListener( 'exit' );
+      process.removeAllListeners( 'exit' );
       process.exit();
     }
   });
@@ -1546,10 +1554,12 @@ function appRepairExitHandler()
 
 let _onExitHandlers = [];
 
-function appRegisterExitHandler( routine )
+function appExitHandlerOnce( routine )
 {
   _.assert( arguments.length === 1 );
   _.assert( _.routineIs( routine ) );
+
+  _.appExitHandlerRepair();
 
   if( typeof process === 'undefined' )
   return;
@@ -1557,8 +1567,8 @@ function appRegisterExitHandler( routine )
   if( !_onExitHandlers.length )
   {
     process.once( 'exit', onExitHandler );
-    process.once( 'SIGINT', onExitHandler );
-    process.once( 'SIGTERM', onExitHandler );
+    // process.once( 'SIGINT', onExitHandler );
+    // process.once( 'SIGTERM', onExitHandler );
   }
 
   _onExitHandlers.push( routine );
@@ -1578,13 +1588,69 @@ function appRegisterExitHandler( routine )
         _.errLogOnce( err );
       }
     })
-
     process.removeListener( 'exit', onExitHandler );
-    process.removeListener( 'SIGINT', onExitHandler );
-    process.removeListener( 'SIGTERM', onExitHandler );
+    _onExitHandlers.splice( 0, _onExitHandlers.length );
+    // process.removeListener( 'SIGINT', onExitHandler );
+    // process.removeListener( 'SIGTERM', onExitHandler );
   }
 
 }
+
+//
+
+/*
+qqq : cover routine appExitHandlerOff by tests
+*/
+
+function appExitHandlerOff( routine )
+{
+  _.assert( arguments.length === 1 );
+  _.assert( _.routineIs( routine ) );
+
+  debugger;
+
+  return _.arrayRemovedElement( _onExitHandlers, routine );
+}
+
+// function appExitHandlerOnce( routine )
+// {
+//   _.assert( arguments.length === 1 );
+//   _.assert( _.routineIs( routine ) );
+//
+//   if( typeof process === 'undefined' )
+//   return;
+//
+//   if( !_onExitHandlers.length )
+//   {
+//     process.once( 'exit', onExitHandler );
+//     process.once( 'SIGINT', onExitHandler );
+//     process.once( 'SIGTERM', onExitHandler );
+//   }
+//
+//   _onExitHandlers.push( routine );
+//
+//   /*  */
+//
+//   function onExitHandler( arg )
+//   {
+//     _.each( _onExitHandlers, ( routine ) =>
+//     {
+//       try
+//       {
+//         routine( arg );
+//       }
+//       catch( err )
+//       {
+//         _.errLogOnce( err );
+//       }
+//     })
+//
+//     process.removeListener( 'exit', onExitHandler );
+//     process.removeListener( 'SIGINT', onExitHandler );
+//     process.removeListener( 'SIGTERM', onExitHandler );
+//   }
+//
+// }
 
 //
 
@@ -1622,8 +1688,9 @@ let Proto =
   appExit,
   appExitWithBeep,
 
-  appRepairExitHandler,
-  appRegisterExitHandler,
+  appExitHandlerRepair,
+  appExitHandlerOnce,
+  appExitHandlerOff,
 
   appMemoryUsageInfo,
 
