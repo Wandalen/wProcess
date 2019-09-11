4f994b46171f321c667fba5da82336917a0e1276
vmigdal <volodymyr.migdal@gmail.com>
2019-09-11 19:35:34 +0300

    add experiment

---
+++ sample/experiment/App.js
@@ -0,0 +1,10 @@
+if( typeof module !== 'undefined' )
+var _ = require( '..' );
+
+_.include( 'wFiles' )
+
+while( 1 )
+{
+  console.log( 'read...' )
+  _.fileProvider.fileRead( __filename )
+}
---
+++ sample/experiment/ShellExperiment.js
@@ -0,0 +1,23 @@
+if( typeof module !== 'undefined' )
+require( '..' );
+require( 'wFiles' );
+var _ = wTools;
+
+let o =
+{ mode : 'fork', execPath : _.path.join( __dirname, 'App.js' ), throwingExitCode :1 }
+
+var ready = _.shell( o )
+ready.finally( ( err, got ) =>
+{
+  _.errAttend( err );
+  debugger
+  console.log( o.exitCode )
+  console.log( o.exitSignal )
+  return null;
+})
+
+_.timeOut( 3000, () =>
+{
+  o.process.kill( 'SIGINT' );
+  return null;
+})
