8e1dfe13676967ad5da86ea5f02a7b550aeccfa5
Volodymyr M <volodymyr.migdal@gmail.com>
2019-12-10 17:25:39 +0200

    update experiment

--- sample/Parent.js
+++ sample/Parent.js
@@ -5,12 +5,25 @@ var _ = wTools;
 
 /* How to execute command asynchronously */
 
+let signal = process.argv[ 2 ];
+
+console.log( 'SIGNAL:', signal )
+
 let o = { execPath : 'node sample/Child.js', mode : 'shell', throwingExitCode : 0, outputPiping : 1 };
 var ready = _.process.start( o )
 
 _.time.out( 1000, () => 
 {
-  o.process.kill( 'SIGINT' );
+  o.process.kill( signal );
+})
+
+_.time.out( 3000, () => 
+{
+  if( _.process.isRunning( o.process.pid ) )
+  { 
+    console.log( 'Child is still running after sending:', signal )
+    o.process.kill('SIGKILL');
+  }
 })
 
 ready.then( ( got ) => 
