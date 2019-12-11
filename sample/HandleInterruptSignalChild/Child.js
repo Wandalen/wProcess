d30023d2f502738d6dcc0070f922ace6cd0c3099
vmigdal <volodymyr.migdal@gmail.com>
2019-12-11 10:33:51 +0200

    update sample

--- sample/HandleInterruptSignalChild/Child.js
+++ sample/HandleInterruptSignalChild/Child.js
@@ -4,6 +4,7 @@ console.log( 'Child process start' );
 process.on( 'SIGINT', () => 
 {
   console.log( 'Child process received SIGINT from parent.' );
+  process.exit( 0 );
 })
 setTimeout( () => 
 {
