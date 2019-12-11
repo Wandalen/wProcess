f39d86b4a706bc9387226e84e42284197d40c003
vmigdal <volodymyr.migdal@gmail.com>
2019-12-11 12:40:29 +0200

    windows-kill sample

---
+++ sample/HandleInterruptSignalChild/Windows-kill.js
@@ -0,0 +1,30 @@
+if( typeof module !== 'undefined' )
+
+var windowsKill = require( 'windows-kill' )();
+
+require( '../..' );
+require( 'wFiles' );
+var _ = wTools;
+
+let o = 
+{ 
+  execPath : 'node Child.js',
+  currentPath : __dirname,
+  mode : 'spawn', 
+  throwingExitCode : 0,
+  outputPiping : 1 
+};
+
+var ready = _.process.start( o )
+
+_.time.out( 1000, () => 
+{
+  windowsKill( o.process.pid, 'SIGINT' )
+})
+
+ready.then( ( got ) => 
+{ 
+  console.log( 'Child process exitCode:', got.exitCode );
+  console.log( 'Child process exitSignal:', got.exitSignal );
+  return null;
+})
