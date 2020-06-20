fb52fef1b90a797f0104f31e48ba17196dc3e8e9
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-06-20 18:32:15 +0300

    adjust expected output

--- proto/dwtools/abase/l4.test/ProcessBasic.test.s
+++ proto/dwtools/abase/l4.test/ProcessBasic.test.s
@@ -18923,7 +18923,7 @@ function shellExperiment( test )
     .thenKeep( function()
     {
       test.identical( options.exitCode, 0 );
-      test.identical( options.output, [ '*' ] );
+      test.identical( options.output, `[ '*' ]` );
       return null;
     })
   })
