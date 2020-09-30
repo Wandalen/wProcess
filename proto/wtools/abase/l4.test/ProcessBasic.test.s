47af3a7f07ef6ed20f3d4aff2f4f2aa04b1ff9df
Jack Shell <jackshel45@gmail.com>
2020-09-30 11:51:39 +0300

    Refactored test routine `shellStartingDelay`

--- proto/wtools/abase/l4.test/ProcessBasic.test.s
+++ proto/wtools/abase/l4.test/ProcessBasic.test.s
@@ -9334,25 +9334,12 @@ shellTerminateAfterLoopRelease.description =
 function shellStartingDelay( test )
 {
   let context = this;
-  var routinePath = _.path.join( context.suiteTempPath, test.name );
+  let a = test.assetFor( false );
+  let testAppPath = a.program( testApp );
 
   /* */
 
-  function testApp()
-  {
-    let data = { t2 : _.time.now() };
-    console.log( JSON.stringify( data ) );
-  }
-
-  /* */
-
-  var testAppPath = _.fileProvider.path.nativize( _.path.join( routinePath, 'testApp.js' ) );
-  var testAppCode = context.toolsPathInclude + testApp.toString() + '\ntestApp();';
-  _.fileProvider.fileWrite( testAppPath, testAppCode );
-  testAppPath = _.strQuote( testAppPath );
-  var ready = new _.Consequence().take( null );
-
-  ready
+  a.ready
 
   .then( () =>
   {
@@ -9381,7 +9368,17 @@ function shellStartingDelay( test )
     return con;
   })
 
-  return ready;
+  return a.ready;
+
+  /* - */
+
+  function testApp()
+  {
+    let _ = require( toolsPath );
+
+    let data = { t2 : _.time.now() };
+    console.log( JSON.stringify( data ) );
+  }
 }
 
 //
