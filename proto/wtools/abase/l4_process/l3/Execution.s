b8afe600a6009577c8dc3e0fdc4a1a2e50fa32ac
Jack Shell <jackshel45@gmail.com>
2020-10-22 10:59:28 +0300

    Fixed acc to review

--- proto/wtools/abase/l4_process/l3/Execution.s
+++ proto/wtools/abase/l4_process/l3/Execution.s
@@ -1233,7 +1233,8 @@ function startMinimal_body( o )
     if( o.timeOut && o.sync )
     o2.timeout = o.timeOut;
     o2.windowsHide = !!o.hiding;
-    o2.maxBuffer = o.streamSizeLimit ? o.streamSizeLimit : 1024 * 1024;
+    if( o.streamSizeLimit )
+    o2.maxBuffer = o.streamSizeLimit;
     return o2;
   }
 
@@ -1252,7 +1253,6 @@ function startMinimal_body( o )
     if( o.currentPath )
     o2.cwd = _.path.nativize( o.currentPath );
 
-    o2.maxBuffer = o.streamSizeLimit ? o.streamSizeLimit : 1024 * 1024;
     return o2;
   }
 
