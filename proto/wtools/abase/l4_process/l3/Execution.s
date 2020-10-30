4a5f732f238ccbb4f9dffb4e9240c35de813e2bc
wandalen <wandalen.me@gmail.com>
2020-10-30 18:31:35 +0200

    .

--- proto/wtools/abase/l4_process/l3/Execution.s
+++ proto/wtools/abase/l4_process/l3/Execution.s
@@ -399,6 +399,8 @@ function startMinimal_body( o )
     o.args = o.args.slice();
     o.args = _.arrayAs( o.args );
 
+    /* */
+
     _argsLength = o.args.length;
 
     if( _.strIs( o.execPath ) )
@@ -428,6 +430,8 @@ function startMinimal_body( o )
       if( begin && begin === end )
       o.execPath = _.strInsideOf( o.execPath, begin, end );
     }
+
+    /* */
 
     o.currentPath = _.path.resolve( o.currentPath || '.' );
 
