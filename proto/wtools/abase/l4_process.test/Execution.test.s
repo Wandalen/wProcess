63ee49bf0e6b8636e52c43160ff525da4e0ea13b
Jack Shell <jackshel45@gmail.com>
2020-11-05 12:45:48 +0200

    Extended test routine `startNjsWithReadyDelayStructuralMultiple`

--- proto/wtools/abase/l4_process.test/Execution.test.s
+++ proto/wtools/abase/l4_process.test/Execution.test.s
@@ -16511,6 +16511,23 @@ function startNjsWithReadyDelayStructuralMultiple( test )
         exp2.exitReason = 'normal';
         exp2.ended = true;
 
+        /* exception in njs on Windows :
+          no output from detached process in mode::shell
+        */
+        if( tops.mode !== 'shell' || process.platform !== 'win32' || !tops.detaching )
+        test.identical( op.output, 'program1:begin\n' );
+        test.identical( op.exitCode, 0 );
+        test.identical( op.exitSignal, null );
+        test.identical( op.exitReason, 'normal' );
+        /* exception in njs on Windows :
+          no output from detached process in mode::shell
+        */
+        if( tops.mode !== 'shell' || process.platform !== 'win32' || !tops.detaching )
+        exp2.output = 'program1:begin\n';
+        exp2.exitCode = 0;
+        exp2.exitSignal = null;
+        exp2.exitReason = 'normal';
+
         test.identical( options, exp2 );
         test.is( !options.process );
         test.is( _.streamIs( options.streamOut ) );
