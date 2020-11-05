394f8fc2e66aa5387e892feb0c0bc1ee3f5d59ea
Jack Shell <jackshel45@gmail.com>
2020-11-05 13:52:47 +0200

    update comment

--- proto/wtools/abase/l4_process.test/Execution.test.s
+++ proto/wtools/abase/l4_process.test/Execution.test.s
@@ -165,7 +165,7 @@ Event 'exit' is aways called. Options `stdio` and `detaching` also don't affect 
 
 /* qqq for Yevhen : find all tests with passingThrough:1, separate them from the rest of the test
 and rewrite to run process which run process to avoid influence of arguments of tester on testing
-qqq2 : not done
+qqq2 : not done | aaa : Done.
 */
 
 /*
