6939e7ab0c021ea9dcf2f8d9c61deaaefb8636bb
wandalen <wandalen.me@gmail.com>
2020-11-30 12:16:37 +0200

    .

--- proto/wtools/abase/l4_process/l2/Process.s
+++ proto/wtools/abase/l4_process/l2/Process.s
@@ -65,57 +65,7 @@ let ProcessMinimal = _.Blueprint
 
 let Process = _.Blueprint
 ({
-  typed : _.trait.typed(),
-  // withConstructor : _.trait.typed(),
-  // extendable : _.trait.extendable(),
-
-  // constructor : function constructor1() { _.assert( 0 ) },
-  // Constructor : function constructor2() { _.assert( 0 ) },
-
-  execPath : null,
-  currentPath : null,
-  args : null,
-  interpreterArgs : null,
-  passingThrough : 0,
-
-  sync : 0,
-  deasync : 0,
-  when : 'instant', /* instant / afterdeath / time / delay */
-  dry : 0,
-
-  mode : 'shell', /* fork / spawn / shell */
-  stdio : 'pipe', /* pipe / ignore / inherit */
-  ipc : null,
-
-  logger : null,
-  procedure : null,
-  stack : null,
-  sessionId : 0,
-
-  ready : null,
-  conStart : null,
-  conTerminate : null,
-  conDisconnect : null,
-
-  env : null,
-  detaching : 0,
-  hiding : 1,
-  uid : null,
-  gid : null,
-  streamSizeLimit : null,
-  timeOut : null,
-
-  throwingExitCode : 'full', /* [ bool-like, 'full', 'brief' ] */ /* must be on by default */  /* qqq for Yevhen : cover */
-  applyingExitCode : 0,
-
-  verbosity : 2,
-  outputPrefixing : 0,
-  outputPiping : null,
-  outputCollecting : 0,
-  outputAdditive : null, /* qqq for Yevhen : cover the option */
-  outputColoring : 1,
-  outputGraying : 0,
-  inputMirroring : 1,
+  inherit : _.define.inherit( ProcessMinimal ),
 
   disconnect : null,
   _end : null,
