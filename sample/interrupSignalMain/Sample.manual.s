ad352ae42a0c981977f899eff6cbfdebb0c0127b
wandalen <wandalen.me@gmail.com>
2020-11-30 09:43:30 +0200

    .

--- sample/interrupSignalMain/Sample.manual.s
+++ sample/interrupSignalMain/Sample.manual.s
@@ -1,4 +1,4 @@
-const _ = require('../../proto/wtools/abase/l4_process/Basic.s');
+const _ = require('../../proto/wtools/abase/l4_process/module/Basic.s');
 
 if( typeof module !== 'undefined' )
 require( '../..' );
