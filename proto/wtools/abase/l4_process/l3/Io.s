564479d7d6aeb9610fd56ffad246377b282c9a84
dmvict <dm.vict.kr@gmail.com>
2021-05-27 09:57:19 +0300

    Extend routine `input`, improve parsing of vectorized properties

--- proto/wtools/abase/l4_process/l3/Io.s
+++ proto/wtools/abase/l4_process/l3/Io.s
@@ -146,6 +146,11 @@ function _inputInSamFormatNodejs( o )
       if( quotes.ranges.length )
       return e;
 
+      let mapSplits = _.strIsolateLeftOrAll( e, o.keyValDelimeter );
+      if( mapSplits[ 1 ] !== undefined )
+      if( !_.strHas( mapSplits[ 0 ], /\s/ ) )
+      return `${ mapSplits[ 0 ] }:"${ mapSplits[ 2 ] }"`
+
       return `"${e}"`;
     })
     .join( ' ' )
