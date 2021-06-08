97169e27cb5afa7674d4be67e49ea1e7f829228f
dmvict <dm.vict.kr@gmail.com>
2021-06-08 10:04:32 +0300

    Reimplement test routine `onCheckDescriptor`, test namespace `process`

--- proto/wtools/abase/l4_process.test/Basic.test.s
+++ proto/wtools/abase/l4_process.test/Basic.test.s
@@ -874,36 +874,119 @@ function onWithChain( test )
 
 function onCheckDescriptor( test )
 {
-  var self = this;
+  const self = this;
+  const a = test.assetFor( false );
+  a.fileProvider.dirMake( a.abs( '.' ) );
+
+  /* - */
+
+  a.ready.then( () =>
+  {
+    test.case = 'from arguments';
+    return null;
+  });
+  var program = a.program( callbackInArgs );
+  program.start();
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    test.identical( _.strCount( op.output, '[ \'available\' ]' ), 1 );
+    test.identical( _.strCount( op.output, '[ \'off\', \'enabled\', \'first\', \'callbackMap\' ]' ), 1 );
+    test.identical( _.strCount( op.output, 'descriptor.enabled : true' ), 1 );
+    test.identical( _.strCount( op.output, 'descriptor.first : false' ), 1 );
+    test.identical( _.strCount( op.output, 'descriptor.callbackMap : available' ), 1 );
+    return null;
+  });
 
   /* */
 
-  test.case = 'call with arguments';
-  var result = [];
-  var onEvent = () => result.push( result.length );
-  var descriptor = _.process.on( 'uncaughtError', onEvent );
-  test.identical( _.props.keys( descriptor ), [ 'uncaughtError' ] );
-  test.identical( _.props.keys( descriptor.uncaughtError ), [ 'off', 'enabled', 'first', 'callbackMap' ] );
-  test.identical( descriptor.uncaughtError.enabled, true );
-  test.identical( descriptor.uncaughtError.first, 0 );
-  test.equivalent( descriptor.uncaughtError.callbackMap, { uncaughtError : onEvent } );
-  test.true( _.event.eventHasHandler( _.process._edispatcher, { eventName : 'uncaughtError', eventHandler : onEvent } ) );
-  descriptor.uncaughtError.off();
+  a.ready.then( () =>
+  {
+    test.case = 'from map';
+    return null;
+  });
+  var program = a.program( callbackInMap );
+  program.start();
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    test.identical( _.strCount( op.output, '[ \'available\' ]' ), 1 );
+    test.identical( _.strCount( op.output, '[ \'off\', \'enabled\', \'first\', \'callbackMap\' ]' ), 1 );
+    test.identical( _.strCount( op.output, 'descriptor.enabled : true' ), 1 );
+    test.identical( _.strCount( op.output, 'descriptor.first : true' ), 1 );
+    test.identical( _.strCount( op.output, 'descriptor.callbackMap : available' ), 1 );
+    return null;
+  });
+
+  /* - */
+
+  return a.ready;
 
   /* */
 
-  test.case = 'call with arguments';
-  var result = [];
-  var onEvent = () => result.push( result.length );
-  var descriptor = _.process.on({ callbackMap : { 'uncaughtError' : onEvent } });
-  test.identical( _.props.keys( descriptor ), [ 'uncaughtError' ] );
-  test.identical( _.props.keys( descriptor.uncaughtError ), [ 'off', 'enabled', 'first', 'callbackMap' ] );
-  test.identical( descriptor.uncaughtError.enabled, true );
-  test.identical( descriptor.uncaughtError.first, 0 );
-  test.equivalent( descriptor.uncaughtError.callbackMap, { uncaughtError : onEvent } );
-  test.true( _.event.eventHasHandler( _.process._edispatcher, { eventName : 'uncaughtError', eventHandler : onEvent } ) );
-  descriptor.uncaughtError.off();
+  function callbackInArgs()
+  {
+    const _ = require( toolsPath );
+    _.include( 'wProcess' );
+    const result = [];
+    const descriptor = _.process.on( 'available', ( ... args ) => result.push( args ) );
+    console.log( _.props.keys( descriptor ) );
+    console.log( _.props.keys( descriptor.available ) );
+    console.log( `descriptor.enabled : ${ descriptor.available.enabled }` );
+    console.log( `descriptor.first : ${ descriptor.available.first }` );
+    console.log( `descriptor.callbackMap : ${ _.props.keys( descriptor.available.callbackMap ) }` );
+  }
+
+  /* */
+
+  function callbackInMap()
+  {
+    const _ = require( toolsPath );
+    _.include( 'wProcess' );
+    const result = [];
+    const descriptor = _.process.on({ callbackMap : { 'available' : ( ... args ) => result.push( args ) }, first : true });
+    console.log( _.props.keys( descriptor ) );
+    console.log( _.props.keys( descriptor.available ) );
+    console.log( `descriptor.enabled : ${ descriptor.available.enabled }` );
+    console.log( `descriptor.first : ${ descriptor.available.first }` );
+    console.log( `descriptor.callbackMap : ${ _.props.keys( descriptor.available.callbackMap ) }` );
+  }
 }
+
+// //
+//
+// function onCheckDescriptor( test )
+// {
+//   var self = this;
+//
+//   /* */
+//
+//   test.case = 'call with arguments';
+//   var result = [];
+//   var onEvent = () => result.push( result.length );
+//   var descriptor = _.process.on( 'uncaughtError', onEvent );
+//   test.identical( _.props.keys( descriptor ), [ 'uncaughtError' ] );
+//   test.identical( _.props.keys( descriptor.uncaughtError ), [ 'off', 'enabled', 'first', 'callbackMap' ] );
+//   test.identical( descriptor.uncaughtError.enabled, true );
+//   test.identical( descriptor.uncaughtError.first, 0 );
+//   test.equivalent( descriptor.uncaughtError.callbackMap, { uncaughtError : onEvent } );
+//   test.true( _.event.eventHasHandler( _.process._edispatcher, { eventName : 'uncaughtError', eventHandler : onEvent } ) );
+//   descriptor.uncaughtError.off();
+//
+//   /* */
+//
+//   test.case = 'call with arguments';
+//   var result = [];
+//   var onEvent = () => result.push( result.length );
+//   var descriptor = _.process.on({ callbackMap : { 'uncaughtError' : onEvent } });
+//   test.identical( _.props.keys( descriptor ), [ 'uncaughtError' ] );
+//   test.identical( _.props.keys( descriptor.uncaughtError ), [ 'off', 'enabled', 'first', 'callbackMap' ] );
+//   test.identical( descriptor.uncaughtError.enabled, true );
+//   test.identical( descriptor.uncaughtError.first, 0 );
+//   test.equivalent( descriptor.uncaughtError.callbackMap, { uncaughtError : onEvent } );
+//   test.true( _.event.eventHasHandler( _.process._edispatcher, { eventName : 'uncaughtError', eventHandler : onEvent } ) );
+//   descriptor.uncaughtError.off();
+// }
 
 //
 
